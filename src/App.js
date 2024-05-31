f085507c185ea9aa4bb73687a50a9a027fe81c0e
Selin ATMACA <selinatmaca@Selins-MacBook-Air.local>
2024-05-31 18:27:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./components/Form";
+import Scoops from "./components/Scoops";
+import Toppings from "./components/Toppings";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black min-vh-100 text-light p-3 py-5">
+      <div className="container">
+        <Scoops />
+        <Toppings />
+        <Form />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
